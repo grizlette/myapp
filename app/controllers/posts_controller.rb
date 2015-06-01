731bcb41208985aa435026a014cf8ae857f61b7b
Megan Grisley <megangrisley@gmail.com>
2015-06-01 14:53:31 -0600

    Created post controller action for a new post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,22 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :author, :body)
+  end
+
+
 end
