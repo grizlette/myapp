ad6742f4e2cea3a71c86464135c503ea12bb7eb9
Megan Grisley <megangrisley@gmail.com>
2015-06-01 15:49:27 -0600

    Added edit and destroy methods and stuff..

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,20 @@
 class PostsController < ApplicationController
+  
+
   def index
     @posts = Post.all
   end
+
 
   def show
     @post = Post.find(params[:id])
   end
 
+
   def new
     @post = Post.new
   end
+
 
   def create
     @post = Post.new(post_params)
@@ -21,9 +26,22 @@ class PostsController < ApplicationController
     end
   end
 
+
   def edit
     @post = Post.find(params[:id])
   end
+
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
 
   private
 
